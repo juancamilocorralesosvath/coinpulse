2dae36549281b1a911d2943a0b9d305ddf3ad12e
juancamilocorralesosvath <juancamilocorra@gmail.com>
2026-01-09 18:46:22 -0500

    feat: add top categories

--- components/home/TrendingCoins.tsx
+++ components/home/TrendingCoins.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { fetcher } from '@/lib/coingecko.actions';
 import Link from 'next/link';
 import Image from 'next/image';
-import { cn, formatCurrency } from '@/lib/utils';
+import { cn, formatCurrency, formatPercentage } from '@/lib/utils';
 import { TrendingDown, TrendingUp } from 'lucide-react';
 import DataTable from '@/components/DataTable'
 import { TrendingCoinsFallback } from './fallback'
@@ -14,58 +14,59 @@ const TrendingCoins = async () => {
 
     try {
         trendingCoins = await fetcher<{ coins: TrendingCoin[] }>('search/trending', undefined, 300);
-     } catch (err) {
+    } catch (err) {
         console.error('TrendingCoins fetch error:', err)
         return <TrendingCoinsFallback />
     }
 
-        const columns: DataTableColumn<TrendingCoin>[] = [
-            {
-                header: 'Name',
-                cellClassName: 'name-cell',
-                cell: (coin) => {
-                    const item = coin.item;
+    const columns: DataTableColumn<TrendingCoin>[] = [
+        {
+            header: 'Name',
+            cellClassName: 'name-cell',
+            cell: (coin) => {
+                const item = coin.item;
 
-                    return (
-                        <Link href={`/coins/${item.id}`} className="flex items-center gap-3">
-                            <Image
-                                src={item.large}
-                                alt={item.name}
-                                width={36}
-                                height={36}
-                            />
-                            <p>{item.name}</p>
+                return (
+                    <Link href={`/coins/${item.id}`} className="flex items-center gap-3">
+                        <Image
+                            src={item.large}
+                            alt={item.name}
+                            width={36}
+                            height={36}
+                        />
+                        <p>{item.name}</p>
 
-                        </Link>
-                    )
-                }
-            },
-            {
-                header: '24h Change',
-                cellClassName: 'name-cell',
-                cell: (coin) => {
-                    const item = coin.item;
-                    const isTrendingUp = item.data.price_change_percentage_24h.usd > 0;
-
-                    return (
-                        <div className={cn('price-change', isTrendingUp ? 'text-green-500' : 'text-red-500')}>
-                            <p>
-                                {isTrendingUp ? (
-                                    <TrendingUp width={16} height={16} />
-                                ) : (<TrendingDown width={16} height={16} />)}
-                                {Math.abs(item.data.price_change_percentage_24h.usd).toFixed(2)}%
-                            </p>
-                        </div>
-                    )
-                }
-            },
-            {
-                header: 'Price',
-                cellClassName: 'price-cell',
-                cell: (coin) => coin.item.data.price
+                    </Link>
+                )
             }
-        ];
-   
+        },
+        {
+            header: '24h Change',
+            cellClassName: 'name-cell',
+            cell: (coin) => {
+                const item = coin.item;
+                const isTrendingUp = item.data.price_change_percentage_24h.usd > 0;
+
+                return (
+                    <div className={cn('price-change', isTrendingUp ? 'text-green-500' : 'text-red-500')}>
+                        <p className='flex items-center'>
+                            {formatPercentage(item.data.price_change_percentage_24h.usd)}
+                            {isTrendingUp ? (
+                                <TrendingUp width={16} height={16} />
+                            ) : (<TrendingDown width={16} height={16} />)}
+
+                        </p>
+                    </div>
+                )
+            }
+        },
+        {
+            header: 'Price',
+            cellClassName: 'price-cell',
+            cell: (coin) => coin.item.data.price
+        }
+    ];
+
 
     const trendingData: TrendingCoin[] = [
         {
